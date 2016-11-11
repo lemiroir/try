4370b5266382eec6fcc37b1a1d3776f6e7352fe1
lemiroir <ismingxing@163.com>
2016-11-11 10:08:17 +0800

    creat a new project

---
+++ code2/test.js
@@ -0,0 +1,19 @@
+// var box = 'Mr.Lee';
+// alert(box.length);
+// alert(box.constructor);
+
+
+var box = 'Mr.Lee';
+alert(box.charAt(4));
+alert(box.charCodeAt(4));
+alert(box.concat(' is',' teacher'));
+
+
+
+
+
+
+
+
+
+
